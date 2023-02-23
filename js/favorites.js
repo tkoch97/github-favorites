c0f9dcbb16a1593c62aab60321991a56dd21a957
Thiago Koch <tkoch.gim@gmail.com>
2023-02-23 17:09:08 -0300

    feat: create HTML with JS

---
+++ js/favorites.js
@@ -0,0 +1,107 @@
+// Criar uma classe para conter a lógica dos dados e como os dados serão estruturados
+
+export class Favorites {
+  constructor(root) {
+    this.root = document.querySelector(root)
+    this.loadData()
+  }
+  
+  loadData() {
+    this.entries = [
+      {
+        login:'tkoch97',
+        name:'Thiago Koch',
+        public_repos: '33',
+        followers: '26'
+      },
+      {
+        login:'birobirobiro',
+        name:'João Inácio Neto',
+        public_repos: '89',
+        followers: '1700'
+      }
+    ]
+  }
+
+  delete(user) {
+    const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
+
+    console.log(filteredEntries)
+  }
+
+}
+
+// Criar outra classe para criar a visualização e os eventos do HTML
+
+// A classe FavoritesView está extendendo a Favorites, assim herdando seus atributos.
+// O super funciona como uma espécie de "copia e cola", onde pega o constructor do Favorites e faz ele existir tbm no FavoritesView.
+//O "querySelectorAll" vai funcionar como uma arrayLike, ou seja, semelhante a uma array, onde podemos tbm aplicar propriedades comuns as arrays.
+// Utilizei o "forEach (para cada)" para criar uma função que vai remover cada "tr" presente no "tbody"
+// criando a função "RemoveAllTr" separada e chamando ela dentro da "update", usamos o conceito de clean code. Onde uma função tem sua tarefa específica e recebe um nome exclarecedor sobre o que faz.
+// No createRow criei uma variável "tr" que tem como valor a criação de um elemento tr no documento HTML, que logo abaixo recebe a propriedade "innerHTML", responsável por introduzir um conteúdo HTML no tr.
+// O "this.entries.forEach" pega cada um dos objetos contidos na array 'entries (entradas)', nomeia cada um como "user" e cria uma variável "row" que executa a função "createRow" e muda o valor de cada elemento HTML baseado nos dados dos users
+// O comando "this.tbody.append (acrescentar)" acrescenta cada linha criada, baseada nos users, dentro do tbody, formando assim as linhas da tabela.
+
+export class FavoritesView extends Favorites {
+  constructor(root){
+    super(root) //aqui estamos apenas selecionando o root (no nosso caso, a div #app).
+
+    this.tbody = this.root.querySelector('table tbody')
+
+    this.update()
+  }
+
+  update() {
+    this.removeAllTr()
+    this.entries.forEach(user => {
+      const row = this.createRow()
+      row.querySelector('.user img').src= `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt= `imagem de ${user.name}`
+      row.querySelector('.user a').href= `https://github.com/${user.login}`
+      row.querySelector('.user p').innerText= `${user.name}`
+      row.querySelector('.user span').innerText= `${user.login}`
+      row.querySelector('.repositories').innerText= `${user.public_repos}`
+      row.querySelector('.followers').innerText= `${user.followers}`
+      row.querySelector('.trash-button').onclick = () => {
+        const isSure = confirm('Tem certeza que deseja deletar esse favorito? 🗑')
+        if(isSure) {
+          this.delete(user)
+        }
+      }
+      this.tbody.append(row)
+    })
+  }
+
+  createRow() {
+    const tr = document.createElement('tr') // variável criada para criar uma linha (o elemento tr) no documento de HTML
+
+    tr.innerHTML = `
+      <tr>
+        <td class="user">
+          <img class="userImg" src="https://github.com/tkoch97.png" alt="Imagem de tkoch97">
+          <a href="https://github.com/tkoch97" target="_blank">
+            <p>Thiago Koch</p>
+            <span>tkoch97</span>
+          </a>
+        </td>
+        <td class="repositories">32</td>
+        <td class="followers">25</td>
+        <td>
+          <button class="trash-button">
+            <svg width="18" height="18" viewBox="0 0 16 16" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="si-glyph si-glyph-trash">
+              <g transform="translate(1.000000, 0.000000)" fill="#E1E1E6">
+                <path d="M0.982,5.073 L2.007,15.339 C2.007,15.705 2.314,16 2.691,16 L10.271,16 C10.648,16 10.955,15.705 10.955,15.339 L11.98,5.073 L0.982,5.073 L0.982,5.073 Z M7.033,14.068 L5.961,14.068 L5.961,6.989 L7.033,6.989 L7.033,14.068 L7.033,14.068 Z M9.033,14.068 L7.961,14.068 L8.961,6.989 L10.033,6.989 L9.033,14.068 L9.033,14.068 Z M5.033,14.068 L3.961,14.068 L2.961,6.989 L4.033,6.989 L5.033,14.068 L5.033,14.068 Z" class="si-glyph-fill"></path>
+                <path d="M12.075,2.105 L8.937,2.105 L8.937,0.709 C8.937,0.317 8.481,0 8.081,0 L4.986,0 C4.586,0 4.031,0.225 4.031,0.615 L4.031,2.011 L0.886,2.105 C0.485,2.105 0.159,2.421 0.159,2.813 L0.159,3.968 L12.8,3.968 L12.8,2.813 C12.801,2.422 12.477,2.105 12.075,2.105 L12.075,2.105 Z M4.947,1.44 C4.947,1.128 5.298,0.875 5.73,0.875 L7.294,0.875 C7.726,0.875 8.076,1.129 8.076,1.44 L8.076,2.105 L4.946,2.105 L4.946,1.44 L4.947,1.44 Z" class="si-glyph-fill"></path>
+              </g>
+            </svg>
+          </button>
+        </td>
+      </tr>
+    `
+    return tr
+  }
+  
+  removeAllTr() {
+    this.tbody.querySelectorAll('tr').forEach((tr) => {tr.remove()})
+  }
+}
